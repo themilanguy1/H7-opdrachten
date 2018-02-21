0d81aa5e2b58786da5017602fc7e96aa66f7e122
themilanguy1 <milan.gupta@ymail.com>
2018-02-21 11:42:32 +0100

    v

--- opdracht-1.php
+++ opdracht-1.php
@@ -12,18 +12,23 @@
         <br>
         <input type="radio" name="BTW" value="1.21">Hoog, 21%
         <br>
-        <input type="submit">
+        <input type="submit" value="uitrekenen">
 
     </form>
 
     <?php
+
+    if(isset($_GET['exclBTW'])) {
         $BTW=$_GET['BTW'];
         $exclBTW=$_GET['exclBTW'];
         $totaal=$BTW * $exclBTW;
         $BTWvar=$totaal-$exclBTW;
+        $btwnummer=$_GET['BTW'] - 1;
 
-        echo $BTWvar ."<br>";
-        echo $totaal ."<br>";
+        echo "Bedrag inclusief &euro;". $BTWvar." BTW bij ". $btwnummer."% BTW: &euro; ". $totaal ."<br>";
+
+    } 
+       
 
 
     ?>
